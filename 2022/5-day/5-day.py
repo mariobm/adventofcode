f2b4551ab88807ba3354fed9953c059114e36fa3
Mario <mbalukcic@plume.com>
2023-04-03 18:41:25 +0200

    chore: optimize Python file for cargo movement (shorten variable names and function parameters, use list comprehensions, combine some lines of code, remove unnecessary underscores)

--- 2022/5-day/5-day.py
+++ 2022/5-day/5-day.py
@@ -3,42 +3,38 @@ import sys
 
 def main():
     with open(sys.argv[1]) as f:
-        cargoPositions, instructions = f.read().split('\n\n')
-    print("Part 1: {}".format(part1(cargoPositions, instructions)))
-    print("Part 2: {}".format(part2(cargoPositions, instructions)))
+        cargo, instructions = f.read().split('\n\n')
+    print("Part 1: {}".format(part1(cargo, instructions)))
+    print("Part 2: {}".format(part2(cargo, instructions)))
 
-def part1(cargoPositions, instructions):
-    cargoYSize = int(cargoPositions.splitlines()[-1][-2])
-    cargoXSize = len(cargoPositions.splitlines()[:-1])
-    cargoStacks = [[None for x in range(cargoXSize)] for y in range(cargoYSize)]
-    for idx, char in enumerate(cargoPositions):
+def part1(cargo, instructions):
+    y = int(cargo.splitlines()[-1][-2])
+    x = len(cargo.splitlines()[:-1])
+    s = [[None for x in range(x)] for y in range(y)]
+    for idx, char in enumerate(cargo):
         if ('A' <= char <= 'Z'):
-            cargoStacks[(idx // 4) % cargoYSize][(idx // 4) // cargoYSize] = char
-    cargoStacks = [list(filter(None, reversed(x))) for x in cargoStacks]
-    for instruction in instructions.splitlines():
-        _move, qty, _from, fromStack, _to, toStack = instruction.split()
-        qty = int(qty)
-        fromStack = int(fromStack) - 1
-        toStack = int(toStack) - 1
-        for _ in range(qty):
-            cargoStacks[toStack].append(cargoStacks[fromStack].pop())
-    return ''.join([cargoStacks[i][-1] for i in range(len(cargoStacks))])
+            s[(idx // 4) % y][(idx // 4) // y] = char
+    s = [list(filter(None, reversed(x))) for x in s]
+    for i in instructions.splitlines():
+        m, q, f, fs, t, ts = i.split()
+        q, fs, ts = int(q), int(fs) - 1, int(ts) - 1
+        for _ in range(q):
+            s[ts].append(s[fs].pop())
+    return ''.join([s[i][-1] for i in range(len(s))])
 
-def part2(cargoPositions, instructions):
-    cargoStacks = [[line[i:i+4].strip() for i in range(0, len(line), 4)] for line in cargoPositions.split('\n')[:-1]]
-    cargoStacks = [[cargoStacks[i][j] for i in range(len(cargoStacks))] for j in range(len(cargoStacks[0]))]
-    cargoStacks = [list(filter(None, reversed(line))) for line in cargoStacks]
-    for instruction in instructions.splitlines():
-        _move, qty, _from, fromStack, _to, toStack = instruction.split()
-        qty = int(qty)
-        fromStack = int(fromStack) - 1
-        toStack = int(toStack) - 1
-        tempStack = []
-        for _ in range(qty):
-            tempStack.append(cargoStacks[fromStack].pop())
-        for _ in range(qty):
-            cargoStacks[toStack].append(tempStack.pop())
-    return ''.join([cargoStacks[i][-1][1] for i in range(len(cargoStacks))])
+def part2(cargo, instructions):
+    s = [[line[i:i+4].strip() for i in range(0, len(line), 4)] for line in cargo.split('\n')[:-1]]
+    s = [[s[i][j] for i in range(len(s))] for j in range(len(s[0]))]
+    s = [list(filter(None, reversed(line))) for line in s]
+    for i in instructions.splitlines():
+        m, q, f, fs, t, ts = i.split()
+        q, fs, ts, temp = int(q), int(fs) - 1, int(ts) - 1, []
+        for _ in range(q):
+            temp.append(s[fs].pop())
+        for _ in range(q):
+            s[ts].append(temp.pop())
+    return ''.join([s[i][-1][1] for i in range(len(s))])
 
 if __name__ == '__main__':
     main()
+    
